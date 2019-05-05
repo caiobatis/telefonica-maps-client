61c0d39a7e65866b85164fc39e10d11c720d98bf
Caio Batista <caiobatis7@gmail.com>
2019-05-05 18:48:26 -0300

    add new content components

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import Header from '../Header/Header'
+import BannerSlider from '../Banners/BannerSlider'
+import BannerFixed from '../Banners/BannerFixed'
 
 import styles from './Home.scss'
 
 
 class Home extends Component {
   render() {
-
-
     return (
       <div className={styles.home}>
+        <Header/>
+        <div className="content">
+          <div className="sectionBanners">
+          <BannerFixed/>
+          <BannerSlider/>
+          </div>
+          <div className="sectionMaps">
+            <div className="container">
+              <div className="maps">
+                <div className="map"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="footer"></div>
       </div>
     )
   }
