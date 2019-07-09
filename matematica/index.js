c156dbc363b795fd49f6d360c922b47ca8590860
Caio Batista <caiobatis7@gmail.com>
2019-07-09 18:12:02 -0300

    development to tooltips from maps

--- matematica/index.js
+++ matematica/index.js
@@ -1,10 +1,35 @@
 const app = $('#app')
 app.html(`<div class="loader"><div class="lds-ring"><div></div><div></div><div></div><div></div></div></div>`)
 
-
+const color = '#491963'
 const map = [
   {
+    id: 1,
     pages: [
+      {
+        title: 'titulo',
+        content: 'conteudo',
+        tooltips: [
+          {
+            positionX: 150,
+            positionY: 260,
+            value: 'teste tooltop da pagina 1'
+          },
+          {
+            positionX: 260,
+            positionY: 460,
+            value: 'teste tooltop'
+          },
+        ],
+        links: [
+          {
+            positionX: 260,
+            positionY: 150,
+            width: 150,
+            value: 'teste tooltop'
+          }
+        ]
+      },
       {
         title: 'titulo',
         content: 'conteudo',
@@ -12,7 +37,7 @@ const map = [
           {
             positionX: 50,
             positionY: 150,
-            value: 'teste tooltop'
+            value: 'teste tooltop da pagina 2'
           },
           {
             positionX: 260,
@@ -28,24 +53,20 @@ const map = [
             value: 'teste tooltop'
           }
         ]
-      }
-    ]
-  },
-  {
-    pages: [
+      },
       {
         title: 'titulo',
         content: 'conteudo',
         tooltips: [
           {
-            positionX: 50,
-            positionY: 150,
+            positionX: 510,
+            positionY: 650,
             value: 'teste tooltop'
           },
           {
             positionX: 260,
             positionY: 150,
-            value: 'teste tooltop'
+            value: 'teste tooltop da pagina 3'
           },
         ],
         links: [
@@ -56,52 +77,49 @@ const map = [
             value: 'teste tooltop'
           }
         ]
-      }
-    ]
-  },
-  {
-    pages: [
-      {
-        title: 'titulo',
-        content: 'conteudo',
-        tooltips: [
-          {
-            positionX: 50,
-            positionY: 150,
-            value: 'teste tooltop'
-          },
-          {
-            positionX: 260,
-            positionY: 150,
-            value: 'teste tooltop'
-          },
-        ],
-        links: [
-          {
-            positionX: 260,
-            positionY: 150,
-            width: 150,
-            value: 'teste tooltop'
-          }
-        ]
-      }
+      },
     ]
   }
 ]
 
-var createStep = () => { 
-  return map.map((e, i)=> {
-    console.log(e, i)
+const createTooltips = tooltips => {
+  return tooltips.map(e=> {
+    return `
+    <div class="tooltip" style="left: ${e.positionX}px; top: ${e.positionY}px;">
+      <div class="icon"></div>
+      <div class="content">
+        <span>${e.value}</span>
+      </div>
+    </div>    
+    `
+  })
+}
+
+const searchMap = () => {
+  let _map
+  map.map(e => {
+    if(e.id == document.location.search.replace('?id=',''))
+      _map = e
+  })
+  return _map
+}
+
+const createStep = () => {
+  const map = searchMap()
+  return map.pages.map((e, i)=> {
+
+
     return `
       <section class="step ${i==0 ? 'show' : ''}">
-        <h1>Teste ${i}</h1>
+        <h1>Página ${i + 1} - ${e.content}</h1>
+        ${createTooltips(e.tooltips)}
       </section>
     `
   })
 }
 
 const template = 
-`<div class="bg">
+`<div class="bg" style="background-color: ${color}">
   <div class="pc">
     <div class="header">
       <div class="title"></div>
@@ -159,14 +177,12 @@ const html =
   },
   prev: function() {
     var step = stepForm.getStep($(this).closest(".step")) - 1;
-    console.log(step)
     $(".step").removeClass("show")
     $(`[data-step="${step}"]`).addClass("show")
     $(".steps").css("transform", "translateX(-" + step + "00%)");
   },
   next: function() {
     var step = stepForm.getStep($(this).closest(".step")) + 1;
-    console.log(step)
     $(".step").removeClass("show")
     $(`[data-step="${step}"]`).addClass("show")
     $(".steps").css("transform", "translateX(-" + step + "00%)");
